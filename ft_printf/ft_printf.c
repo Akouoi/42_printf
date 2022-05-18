a2d99b86b28047c08b06fdbd23c24575f7e40416
Alice KOUOI <akouoi@e3r11p18.clusters.42paris.fr>
2022-05-18 18:11:50 +0200

    1er essai

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,4 +1,4 @@
-#include "libftprintf.h"
+#include "ft_printf.h"
 
 int	ft_putstr(va_list ap)
 {
@@ -10,64 +10,154 @@ int	ft_putstr(va_list ap)
 	if (!s)
 		return (0);
 	while (s[i])	
-	{
-		write (1, &s[i], 1);
-		i++;
-	}
+		i += write (1, &s[i], 1);
 	return (i);
 }
 int	ft_putchar(va_list ap)
 {
-	char	c;
+	int	c;
 
-	c = va_arg(ap, int) + '0';
-	write(1, &c, 1);
-	return (1);
+	c = va_arg(ap, int);
+	return (write(1, &c, 1));
 }
-static int	ft_nblen(int n)
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	ft_nblen(unsigned long long int n, unsigned long long int base_len)
 {
 	int	power;
-	int	sign;
 
-	sign = 1 - 2 * (n < 0);
 	power = 0;
-	n *= sign;
-	while (n > 0)
+	while (n > base_len)
 	{
 		power++;
-		n /= 10;
+		n /= base_len;
 	}
-	return (power + (sign < 0));
+	return (power + 1);
 }
 
-void	ft_putnbr(int n)
+char	*ft_itoa(int n, char *base)
 {
-	char	*p;
+	char	*str;
+	int		sign;
+	int		n_len;
+	int	base_len = ft_strlen(base);
 
-	p = NULL;
-	if (n == -2147483648)
-		write(1, "-2147483648", 12);
-	if (n < 0)
+	sign = 1 - 2 * (n < 0);
+	n *= sign;
+	n_len = ft_nblen(n, base_len);
+	str = malloc((n_len + 1 + (sign < 0))* sizeof(char));
+	if (!str)
+		return (NULL);
+	str[n_len + (sign < 0)] = 0;
+	while (--n_len + (sign < 0) >= 0)
 	{
-		n *= -1;
-		write(1, "-", 1);
+		str[n_len + (sign < 0)] = base[n % base_len];
+		n /= base_len;
 	}
-	if (n >= 10)
-		ft_putnbr(n / 10);
-	*p = n % 10 + '0';
-	write(1, p, 1);
+	if (sign < 0)
+		str[0] = '-';
+	write(1, str, ft_strlen(str));
+	return (str);
 }
 
-int	ft_nbr(va_list ap)
+int	ft_putnbr_dec(va_list ap)
 {
 	int	n;
+	char	*base = "0123456789";
+	char	*str;
+	int	n_len;
 
 	n = va_arg(ap, int);
-	ft_putnbr(n);
-	return (ft_nblen(n));
+	if (n == -2147483648)
+		return(write(1, "-2147483648", 12));
+	str = ft_itoa(n, base);
+	n_len = ft_strlen(str);
+	free(str);
+	return (n_len);
 }
 
-static int	ft_index(int c)
+
+char	*ft_itoa_base(unsigned long long int n, char *base)
+{
+	char	*str;
+	unsigned long long int	base_len = ft_strlen(base);
+	int	n_len;
+
+	n_len = ft_nblen(n, base_len);
+	str = malloc((n_len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	str[n_len] = 0;
+	while (n_len - 1 >= 0)
+	{
+		str[n_len - 1] = base[n % base_len];
+		n /= base_len;
+		n_len--;
+	}
+	write(1, str, ft_strlen(str));
+	return (str);
+}
+
+int	ft_putnbr_hex(va_list ap)
+{
+	unsigned long long int	n;
+	int	n_len;
+	char *base = "0123456789abcdef";
+	char	*str;
+
+	n = va_arg(ap, unsigned long long int);
+	str = ft_itoa_base(n, base);
+	n_len = ft_strlen(str);
+	free(str);
+	return (n_len);
+}
+
+int	ft_putnbr_hexm(va_list ap)
+{
+	int	n;
+	int	n_len;
+	char *base = "0123456789ABCDEF";
+	char	*str;
+
+	n = va_arg(ap, int);
+	str = ft_itoa_base((unsigned int) n, base);
+	n_len = ft_strlen(str);
+	free(str);
+	return (n_len);
+}
+
+int	ft_point(va_list ap)
+{
+	write(1, "0x", 2);
+	return(2 + ft_putnbr_hex(ap));
+}
+int	ft_format(va_list ap, int i)
+{
+	int	(*fct[9])(va_list);
+
+	fct[0] = ft_putchar;
+	fct[1] = ft_putstr;
+	fct[2] = ft_point;
+	fct[3] = ft_putnbr_dec;
+	fct[4] = ft_putnbr_dec;
+	fct[6] = ft_putnbr_hex;
+	fct[7] = ft_putnbr_hexm;
+
+	return (fct[i](ap));
+}
+
+static int	ft_index(char c)
 {
 	int	i;
 	char	*set;
@@ -80,19 +170,7 @@ static int	ft_index(int c)
 			return (i);
 		i++;
 	}
-	printf("index = %d\n", i);
 	return (i);
-}
-
-int	ft_format(va_list ap, int i)
-{
-	int	(*fct[9])(va_list);
-
-	//fct[0] = ft_putchar;
-	fct[1] = ft_putstr;
-	fct[3] = ft_nbr;
-
-	return (fct[i](ap));
 }
 
 int 	ft_printf(const char *s, ...)
@@ -106,30 +184,31 @@ int 	ft_printf(const char *s, ...)
 	va_start(ap, s);
 	while (*s)
 	{
-		if (*(s - 1) == '%')
+		if (*s == '%')
 		{
-			if (ft_index(*s) < 8)
-			{
-				i += ft_format(ap, ft_index(*s));
-				if (s+1)
-					s++;
-			}
-			else
-			{
-				write (1, s, 1);
-				i++;
-			}
+			if (ft_index(*(s + 1)) < 8)
+				i += ft_format(ap, ft_index(*(s++ + 1)));
+			if (*(s + 1) == '%')
+				i += write (1, s++, 1);
 		}
+		else
+			i += write (1, s, 1);
 		s++;
-
 	}
 	return (i);
 }
 
+#include <limits.h>
 int	main()
 {
-	printf("\n%d\n", printf("%s", "hello"));
-	printf("\n%d\n", ft_printf("%s", "hello"));
-//	printf("\n%d\n", printf("%d", 12));
-//	printf("\n%d\n", ft_printf("%d", 12));
+	char *p = "hello";
+	printf(" | count :%d\n", printf("point : %p\n", &p));
+	printf(" | count :%d\n", ft_printf("point : %p\n", &p));
+	printf(" | count :%d\n", printf("hex : %x\n", 0));
+	printf(" | count :%d\n", ft_printf("hex : %x\n", 0));
+	printf(" | count :%d\n", ft_printf("%%\n"));
+	printf(" | count :%d\n", printf("HEX : %X\n", 0));
+	printf(" | count :%d\n", ft_printf("HEX : %X\n", 0));
+	printf(" | count :%d\n", printf("char - string - dec - int|| %c %s %d %i", 'c', "hello", 2147, -2147483647));
+	printf(" | count :%d\n", ft_printf("char - string - dec - int|| %c %s %d %i", 'c', "hello", 2147, -2147483647));
 }
